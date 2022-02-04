440884fe1d343238e3afaf91da62f040568b3f59
harrisonBarnett <74162979+harrisonBarnett@users.noreply.github.com>
2022-02-04 10:46:58 -0500

    init commit

---
+++ HNReader/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace HNReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reader reader = new Reader();
+            Console.WriteLine("Welcome to HNReader. Enter the command 'get' to retrieve new stories.");
+            string input = Console.ReadLine();
+            string result = "";
+            if(input == "get")
+            {
+                result = reader.Get();
+            }
+            Console.WriteLine(result);
+        }
+
+    }
+    
+    class Reader
+    {
+        public string Get()
+        {
+            return "hello harrison";
+        }
+    }
+}
