0acc7369d58aea87ea584b913f597963ea8529e3
harrisonBarnett <74162979+harrisonBarnett@users.noreply.github.com>
2022-02-08 18:11:40 -0500

    refining switch controllers

--- HNReader/Program.cs
+++ HNReader/Program.cs
@@ -21,7 +21,7 @@ namespace HNReader
                         await GetNewStories();
                         break;
                     case "top":
-                        await GetStoryDetail();
+                        await GetTopStories();
                         break;
                     case "quit":
                         runProgram = false;
@@ -32,6 +32,7 @@ namespace HNReader
             } while (runProgram == true);
 
         }
+        // HOME HEADER AND COMMAND UI
         private static void HomeHeader()
         {
             AnsiConsole.Write(
@@ -42,21 +43,25 @@ namespace HNReader
             table.AddColumn("quit");
             AnsiConsole.Write(table);
         }
+        // NEW STORIES VIEW HEADER AND COMMAND UI
         private static void NewStoriesHeader()
         {
             AnsiConsole.Write(
                 new FigletText("new")
                 );
             var table = new Table();
-            table.AddColumn("fetch one");
+            table.AddColumn("get one");
             table.AddColumn("back");
             AnsiConsole.Write(table);
         }
+        // RETURN NEW STORIES FROM HN
         private static async Task GetNewStories()
         {
-            try{
-                // clear HTTP headers
-                _httpClient.DefaultRequestHeaders.Accept.Clear();
+            NewStoriesHeader();
+            try
+            {
+                //// clear HTTP headers
+                //_httpClient.DefaultRequestHeaders.Accept.Clear();
                 // API endpoint to return the _id of top stories on HN                
                 var response = _httpClient.GetStringAsync("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
                 var storyIDs = await response;
@@ -72,7 +77,7 @@ namespace HNReader
                 table.AddColumn("score");
 
                 for (int i = 0; i < 10; i++) {
-                    StoryAbbreviated story = await GetStoryAbbreviated(jsonData[i]);
+                    Story story = await GetStory(jsonData[i]);
                     string index = i.ToString();
                     DateTime time = UnixTimeToDateTime(story.time);
                     string title = story.title.ToString();
@@ -80,18 +85,16 @@ namespace HNReader
                     table.AddRow(index, time.ToString(), title.EscapeMarkup(), score);
                 }
                 AnsiConsole.Write(table);
+
                 bool runProgram = true;
                 do
                 {
-                    var control = new Table();
-                    control.AddColumn("fetch one");
-                    control.AddColumn("back");
-                    AnsiConsole.Write(control);
                     string input = Console.ReadLine();
                     switch (input)
                     {
-                        case "fetch one":
-                            Console.WriteLine("fetching one story");
+                        case "get one":
+                            // TEST DATA, HARDCODED
+                            GetStoryDetail(8863);
                             break;
                         case "back":
                             runProgram = false;
@@ -105,9 +108,60 @@ namespace HNReader
                 Console.WriteLine("Exception: {0}", e.Message);
             }
         }
-        private static async Task<StoryAbbreviated> GetStoryAbbreviated(Int64 storyID)
+        // RETURN TOP STORIES FROM HN
+        private static async Task GetTopStories()
         {
-            StoryAbbreviated toReturn = null;
+            Console.WriteLine("Reading story detail\nEnter Story Index: ");
+            string input = Console.ReadLine();
+        }
+        // STORY DETAIL VIEW
+        private static async Task GetStoryDetail(Int64 storyID)
+        {
+            Console.WriteLine("Enter story index: ");
+            string input = Console.ReadLine();
+
+            var table = new Table();
+            table.Border(TableBorder.Ascii);
+
+            table.AddColumn("date / time");
+            table.AddColumn("title");
+            table.AddColumn("score");
+
+
+            Story story = await GetStory(storyID);
+
+            DateTime time = UnixTimeToDateTime(story.time);
+            string title = story.title.ToString();
+            string score = story.score.ToString();
+            table.AddRow(time.ToString(), title.EscapeMarkup(), score);
+            AnsiConsole.Write(table);
+
+            bool runProgram = true;
+            do
+            {
+                var control = new Table();
+                control.AddColumn("show comments");
+                control.AddColumn("back");
+                AnsiConsole.Write(control);
+                string command = Console.ReadLine();
+                switch (command)
+                {
+                    case "show comments":
+                        Console.WriteLine("Showing comments");
+                        break;
+                    case "back":
+                        runProgram = false;
+                        break;
+                    default:
+                        break;
+                }
+            } while (runProgram == true);
+
+        }
+        // RETURN A SINGLE STORY FROM HN
+        private static async Task<Story> GetStory(Int64 storyID)
+        {
+            Story toReturn = null;
 
             try
             {
@@ -116,7 +170,7 @@ namespace HNReader
                 var response = _httpClient.GetStringAsync("https://hacker-news.firebaseio.com/v0/item/" + storyID + ".json?print=pretty");
                 var story = await response;
 
-                var jsonData = JsonConvert.DeserializeObject<StoryAbbreviated>(story);
+                var jsonData = JsonConvert.DeserializeObject<Story>(story);
 
                 toReturn = jsonData;
             } catch(HttpRequestException e)
@@ -126,22 +180,21 @@ namespace HNReader
 
             return toReturn;
         }
-        private static async Task GetStoryDetail()
-        {
-            Console.WriteLine("Reading story detail\nEnter Story Index: ");
-            string input = Console.ReadLine();
-        }
+        // CONVERT UNIX TIME TO HUMAN READABLE
         public static DateTime UnixTimeToDateTime(long unixtime)
         {
             System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
             dtDateTime = dtDateTime.AddSeconds(unixtime).ToLocalTime();
             return dtDateTime;
         }
-        public class StoryAbbreviated {
+        // STORY STRUCT
+        public class Story {
             public int time { get; set; }
             public string? title { get; set; }
-            public int score { get; set; }
+            public string? text { get; set; }
 
+            public int score { get; set; }
+            public int[] kids { get; set; }
         }
     }
     
