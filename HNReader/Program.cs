d639952ab110e6f30747e71452bf55a13ff4a084
harrisonBarnett <74162979+harrisonBarnett@users.noreply.github.com>
2022-02-10 11:01:54 -0500

    separating logic and UI, refactoring

--- HNReader/Program.cs
+++ HNReader/Program.cs
@@ -10,183 +10,40 @@ namespace HNReader
 
         public static async Task Main(string[] args)
         {
-            bool runProgram = true;
-            do
+            Reader reader = new Reader();
+
+            while (true)
             {
-                HomeHeader();
+                reader.HomeViewAsync();
+
                 string input = Console.ReadLine();
                 switch(input)
                 {
-                    case "new":
-                        await GetNewStories();
+                    case "prev":
+                        Console.WriteLine("getting previous page");
                         break;
-                    case "top":
-                        await GetTopStories();
+                    case "next":
+                        Console.WriteLine("getting next page"); 
+                        break;
+                    case "get one":
+                        Console.WriteLine("enter story index: ");
+                        string storyIndex = Console.ReadLine();
+                        reader.GetStoryDetail(storyIndex);
+                        break;
+                    case "refresh":
+                        reader.HomeViewAsync();
                         break;
                     case "quit":
-                        runProgram = false;
+                        return;
                         break;
                     default:
                         break;
                 }
-            } while (runProgram == true);
-
-        }
-        // HOME HEADER AND COMMAND UI
-        private static void HomeHeader()
-        {
-            AnsiConsole.Write(
-                new FigletText("HN Reader"));
-            var table = new Table();
-            table.AddColumn("new");
-            table.AddColumn("top");
-            table.AddColumn("quit");
-            AnsiConsole.Write(table);
-        }
-        // NEW STORIES VIEW HEADER AND COMMAND UI
-        private static void NewStoriesHeader()
-        {
-            AnsiConsole.Write(
-                new FigletText("new")
-                );
-            var table = new Table();
-            table.AddColumn("get one");
-            table.AddColumn("back");
-            AnsiConsole.Write(table);
-        }
-        // RETURN NEW STORIES FROM HN
-        private static async Task GetNewStories()
-        {
-            NewStoriesHeader();
-            try
-            {
-                //// clear HTTP headers
-                //_httpClient.DefaultRequestHeaders.Accept.Clear();
-                // API endpoint to return the _id of top stories on HN                
-                var response = _httpClient.GetStringAsync("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
-                var storyIDs = await response;
-
-                var jsonData = JsonConvert.DeserializeObject<List<dynamic>>(storyIDs);
-
-                var table = new Table();
-                table.Border(TableBorder.Ascii);
-
-                table.AddColumn("i");
-                table.AddColumn("date / time");
-                table.AddColumn("title");
-                table.AddColumn("score");
-
-                for (int i = 0; i < 10; i++) {
-                    Story story = await GetStory(jsonData[i]);
-                    string index = i.ToString();
-                    DateTime time = UnixTimeToDateTime(story.time);
-                    string title = story.title.ToString();
-                    string score = story.score.ToString();
-                    table.AddRow(index, time.ToString(), title.EscapeMarkup(), score);
-                }
-                AnsiConsole.Write(table);
-
-                bool runProgram = true;
-                do
-                {
-                    string input = Console.ReadLine();
-                    switch (input)
-                    {
-                        case "get one":
-                            // TEST DATA, HARDCODED
-                            GetStoryDetail(8863);
-                            break;
-                        case "back":
-                            runProgram = false;
-                            break;
-                        default:
-                            break;
-                    }
-                } while (runProgram == true);
-            }
-            catch (HttpRequestException e) {
-                Console.WriteLine("Exception: {0}", e.Message);
-            }
-        }
-        // RETURN TOP STORIES FROM HN
-        private static async Task GetTopStories()
-        {
-            Console.WriteLine("Reading story detail\nEnter Story Index: ");
-            string input = Console.ReadLine();
-        }
-        // STORY DETAIL VIEW
-        private static async Task GetStoryDetail(Int64 storyID)
-        {
-            Console.WriteLine("Enter story index: ");
-            string input = Console.ReadLine();
-
-            var table = new Table();
-            table.Border(TableBorder.Ascii);
-
-            table.AddColumn("date / time");
-            table.AddColumn("title");
-            table.AddColumn("score");
-
-
-            Story story = await GetStory(storyID);
-
-            DateTime time = UnixTimeToDateTime(story.time);
-            string title = story.title.ToString();
-            string score = story.score.ToString();
-            table.AddRow(time.ToString(), title.EscapeMarkup(), score);
-            AnsiConsole.Write(table);
-
-            bool runProgram = true;
-            do
-            {
-                var control = new Table();
-                control.AddColumn("show comments");
-                control.AddColumn("back");
-                AnsiConsole.Write(control);
-                string command = Console.ReadLine();
-                switch (command)
-                {
-                    case "show comments":
-                        Console.WriteLine("Showing comments");
-                        break;
-                    case "back":
-                        runProgram = false;
-                        break;
-                    default:
-                        break;
-                }
-            } while (runProgram == true);
-
-        }
-        // RETURN A SINGLE STORY FROM HN
-        private static async Task<Story> GetStory(Int64 storyID)
-        {
-            Story toReturn = null;
-
-            try
-            {
-                _httpClient.DefaultRequestHeaders.Accept.Clear();
-
-                var response = _httpClient.GetStringAsync("https://hacker-news.firebaseio.com/v0/item/" + storyID + ".json?print=pretty");
-                var story = await response;
-
-                var jsonData = JsonConvert.DeserializeObject<Story>(story);
-
-                toReturn = jsonData;
-            } catch(HttpRequestException e)
-            {
-                Console.WriteLine("Exception {0}", e.Message);
             }
 
-            return toReturn;
         }
-        // CONVERT UNIX TIME TO HUMAN READABLE
-        public static DateTime UnixTimeToDateTime(long unixtime)
-        {
-            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
-            dtDateTime = dtDateTime.AddSeconds(unixtime).ToLocalTime();
-            return dtDateTime;
-        }
+      
+
         // STORY STRUCT
         public class Story {
             public int time { get; set; }
---
+++ HNReader/Reader.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spectre.Console;
+using Newtonsoft.Json;
+
+namespace HNReader
+{
+    internal class Reader
+    {
+        // initialize http client
+        private static HttpClient _httpClient = new HttpClient();
+
+        // list of stories stored in memory when reader is instantiated
+        private static List<Story> _storyList;
+
+
+        // main view upon opening app and returning to home view
+        public async Task HomeViewAsync()
+        {
+            AnsiConsole.Write(new FigletText("HN Reader"));
+
+            await GetStoriesAbbreviated();
+
+            var table = new Table();
+            table.AddColumn("prev");
+            table.AddColumn("next");
+            table.AddColumn("get one");
+            table.AddColumn("refresh");
+            table.AddColumn("quit");
+            AnsiConsole.Write(table);
+
+            
+        }
+
+        // Get abbreviated stories 10 at a time
+        private static async Task GetStoriesAbbreviated()
+        {
+            // TODO: implement pagination?
+            try
+            {
+                // API endpoint to return list of IDs of top stories on HN                
+                var response = _httpClient.GetStringAsync("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
+                var storyIDs = await response;
+                var deserializedIDs = JsonConvert.DeserializeObject<List<dynamic>>(storyIDs);
+
+                var table = new Table();
+                table.Border(TableBorder.Ascii);
+
+                table.AddColumn("i");
+                table.AddColumn("date / time");
+                table.AddColumn("title");
+                table.AddColumn("score");
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Story story = await GetStory(deserializedIDs[i]);
+                    string index = i.ToString();
+                    DateTime time = UnixTimeToDateTime(story.time);
+                    string title = story.title.ToString();
+                    string score = story.score.ToString();
+                    table.AddRow(index, time.ToString(), title.EscapeMarkup(), score);
+                }
+                AnsiConsole.Write(table);
+
+            }
+            catch (HttpRequestException e)
+            {
+                AnsiConsole.Write(new FigletText("ERROR:"));
+                AnsiConsole.Write(new FigletText(e.StatusCode.ToString())); 
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        // Get story detail
+        public async Task GetStoryDetail(string storyIndex)
+        {
+            Console.WriteLine("hello from the get story detail method. fetching story {0}", storyIndex);
+        }
+
+        // Get and deserialize a story to json data
+        private static async Task<Story> GetStory(Int64 storyID)
+        {
+            Story toReturn = null;
+            try
+            {
+                _httpClient.DefaultRequestHeaders.Accept.Clear();
+
+                var response = _httpClient.GetStringAsync("https://hacker-news.firebaseio.com/v0/item/" + storyID + ".json?print=pretty");
+                var story = await response;
+                var jsonData = JsonConvert.DeserializeObject<Story>(story);
+
+                toReturn = jsonData;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("Exception {0}", e.Message);
+            }
+
+            return toReturn;
+        }
+
+        // Convert unix time to human readable
+        private static DateTime UnixTimeToDateTime(long unixtime)
+        {
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixtime).ToLocalTime();
+            return dtDateTime;
+        }
+
+        // // Story model
+        private class Story
+        {
+            public int time { get; set; }
+            public string? title { get; set; }
+            public string? text { get; set; }
+
+            public int score { get; set; }
+            public int[] kids { get; set; }
+        }
+    }
+}
